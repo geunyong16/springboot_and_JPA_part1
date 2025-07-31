d93947f90c0a326a7e8e0fa3130004c2b87aac26
geunyong16 <joyonggeun16@gmail.com>
2025-07-31 15:43:57 +0900

    Lesson 5 Clear

---
+++ src/main/java/study/springboot_and_jpa_part1/SpringbootAndJpaPart1Application.java
@@ -0,0 +1,13 @@
+package study.springboot_and_jpa_part1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndJpaPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAndJpaPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/springboot_and_jpa_part1/SpringbootAndJpaPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot_and_jpa_part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndJpaPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
